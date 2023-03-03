f58fd32a479f14109fd6e0a34e782fad556cdb45
Shezad Hassan <shezadhassan@dhcp-10-22-107-42.wlan.ntnu.no>
2023-03-03 18:09:26 +0100

    Add pickle file and finish first draft

--- scraper.py
+++ scraper.py
@@ -8,6 +8,27 @@ import os
 import csv
 import time # to be deleted
 import requests
+import pickle
+
+# Item class
+class Item:
+    def __init__(self, name, link) -> None:
+        self.name = name
+        self.link = link
+
+def findNewItems(prev_list, new_list):
+    '''
+    prev_list : Last pulled list.
+    new_list : Currently pulled list from the website.
+    Items in both lists are represented by strings of the items url. 
+
+    Returns items in new_list that are not in prev_list. If there are no new items return None. 
+    '''
+    new_items = {}
+    for i in new_list:
+        if i not in prev_list:
+            new_items[i] = new_list[i]
+    return None if new_items == [] else new_items
 
 # Initialise driver
 options = Options()
@@ -34,27 +55,55 @@ print("There are", int(count), "items")
 items_list = driver.find_element("xpath", '//ul[@class="products columns-5"]') 
 items_list = items_list.get_attribute("innerHTML") # Get list of postings in html format
 soup = BeautifulSoup(items_list, 'html.parser') # Create soup object for easy parsing
-items = soup.find_all('li') # Get array of items on nettbutikk
-latest_item = items[0]
-latest_item_name = latest_item.find('h2').get_text()
-latest_item_link = latest_item.find('a').get('href')
-print("link to",latest_item_name, ":", latest_item_link)
+items_html = soup.find_all('li') # Get array of items on nettbutikk
 
-# CSV writing to test cron, to be deleted
-with open('/Users/shezadhassan/Desktop/test.csv', 'a') as f:
-    writer = csv.writer(f)
-    time = time.ctime()
-    towrite = [time + latest_item_name]
-    writer.writerow(towrite)
+# get list of curr_list   maybe use dict instead of list
+curr_items = {}
+for item in items_html:
+    item_name = item.find('h2').get_text()
+    item_link = item.find('a').get('href')
+    curr_items[item_link] = item_name
+    
+# pull prev list from database
+with open("data.pickle", "rb") as f:
+    try:
+        prev_items = pickle.load(f)
+        print("Recieved prev list successfully")
+    except:
+        prev_items = {}
+        print("Prev list reset to empty")
+
+new_items = findNewItems(prev_items, curr_items)
+# if prev_list = curr_list do nothing
+# if curr_list has new item then 1. send slack message 1. update llist in db
+if new_items != None:
+    # Slack message
+    slack_address = 'https://hooks.slack.com/services/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'
+    for i in new_items:
+        payload = '{"text": "A new %s has been posted at %s!"}' % (i ,new_items[i])
+        response = requests.post(
+            slack_address,
+            data = payload
+        )
+        print("Message sent to slack with status: ", response.text)
+
+    # Write new list to db
+    with open("data.pickle", "wb") as f:
+        pickle.dump(curr_items, f)
 
 # Close web driver
 driver.quit()
 
-# Slack message
-slack_address = 'https://hooks.slack.com/services/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'
-payload = '{"text": "%s"}' % latest_item_name
-response = requests.post(
-    slack_address,
-    data = payload
-)
-print(response.text)
+
+'''
+# Write last item to db
+path_to_db = "/Users/shezadhassan/Desktop/Orbital/Nettbutikk Webscraper/nettbutikk-webscraper/database.csv"
+with open(path_to_db, 'w') as f:
+    writer = csv.writer(f)
+    time = time.ctime()
+    #towrite = latest_item_name
+    writer.writerow([items])
+    writer.writerow([time])
+
+
+'''
